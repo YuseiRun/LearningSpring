bc2af389e5dc6c39af8634b83aa03a61daccddcc
Yusei Run <cwheil@outlook.com>
2022-03-28 14:06:09 -0400

    first commit

---
+++ src/main/java/com/puralsight/fundementals/FundementalsApplication.java
@@ -0,0 +1,13 @@
+package com.puralsight.fundementals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundementalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundementalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/puralsight/fundementals/FundementalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puralsight.fundementals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundementalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
