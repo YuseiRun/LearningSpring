90a40522b20f1b3c622c11ff2b969418a3c7dfd6
Yusei Run <cwheil@outlook.com>
2022-07-26 11:21:25 -0400

    change

--- src/main/java/com/puralsight/fundementals/FundementalsApplication.java
+++ src/main/java/com/puralsight/fundementals/FundementalsApplication.java
@@ -2,12 +2,29 @@ package com.puralsight.fundementals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FundementalsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundementalsApplication.class, args);
 	}
+
+	@GetMapping
+	public String hello(){
+		return "hello world";
+	}
+	
+	@GetMapping("/api/foos")
+	@ResponseBody
+	public String getFoos(@RequestParam String id) {
+    	return "ID: " + id;
+	}
+
 
 }
